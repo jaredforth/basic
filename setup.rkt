f197293899316f213715b82e019e2e27d9869be3
Jared Forth <jared.forth@output.com>
2023-04-06 10:42:50 -0500

    add repl to setup

--- setup.rkt
+++ setup.rkt
@@ -1,8 +1,11 @@
 #lang br
+(require "parser.rkt" "tokenizer.rkt")
 (provide basic-output-port do-setup!)
 
 (define basic-output-port
   (make-parameter (open-output-nowhere)))
+
+(define repl-parse (make-rule-parser b-repl))
 
 (define (do-setup!)
   (basic-output-port (current-output-port)))
