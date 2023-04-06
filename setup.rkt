bbf67db7a36ccdee8ce5c57980f0ce384737c7e6
Jared Forth <jared.forth@output.com>
2023-04-06 10:44:28 -0500

    create repl for basic

--- setup.rkt
+++ setup.rkt
@@ -7,5 +7,13 @@
 
 (define repl-parse (make-rule-parser b-repl))
 
+(define (read-one-line origin port)
+  (define one-line (read-line port))
+  (if (eof-object? one-line)
+      eof
+      (repl-parse
+       (make-tokenizer (open-input-string one-line)))))
+
 (define (do-setup!)
-  (basic-output-port (current-output-port)))
+  (basic-output-port (current-output-port))
+  (current-read-interaction read-one-line))
