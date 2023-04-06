f74962640159a42b7bb94ab99552c1ebcf1f193b
Jared Forth <jared.forth@output.com>
2023-04-06 10:26:11 -0500

    update parser

--- parser.rkt
+++ parser.rkt
@@ -2,18 +2,23 @@
 b-program : [b-line] (/NEWLINE [b-line])*
 b-line : b-line-num [b-statement] (/":" [b-statement])* [b-rem]
 @b-line-num : INTEGER
+b-rem : REM
 @b-statement : b-end | b-print | b-goto
              | b-let | b-input | b-if
-b-rem : REM
+             | b-gosub | b-return | b-for | b-next
 b-end : /"end"
 b-print : /"print" [b-printable] (/";" [b-printable])*
 @b-printable : STRING | b-expr
 b-goto : /"goto" b-expr
-b-let : [/"let"] b-id /"=" (b-expr | STRING)
+b-let : [/"let"] b-id /"=" (STRING | b-expr)
 b-if : /"if" b-expr /"then" (b-statement | b-expr)
                    [/"else" (b-statement | b-expr)]
 b-input : /"input" b-id
 @b-id : ID
+b-gosub : /"gosub" b-expr
+b-return : /"return"
+b-for : /"for" b-id /"=" b-expr /"to" b-expr [/"step" b-expr]
+b-next : /"next" b-id
 b-expr : b-or-expr
 b-or-expr : [b-or-expr "or"] b-and-expr
 b-and-expr : [b-and-expr "and"] b-not-expr
