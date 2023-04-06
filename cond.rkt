7f28b1d4549608adece31fd07ca822250bd40244
Jared Forth <jared.forth@output.com>
2023-04-06 10:24:12 -0500

    add conditional module

--- cond.rkt
+++ cond.rkt
@@ -18,3 +18,24 @@
 (define-macro-cases b-not-expr
   [(_ VAL) #'VAL]
   [(_ "not" VAL) #'(if (nonzero? VAL) 0 1)])
+
+(define b= (compose1 bool->int =))
+(define b< (compose1 bool->int <))
+(define b> (compose1 bool->int >))
+(define b<> (compose1 bool->int not =))
+
+(define-macro-cases b-comp-expr
+  [(_ VAL) #'VAL]
+  [(_ LEFT "=" RIGHT) #'(b= LEFT RIGHT)]
+  [(_ LEFT "<" RIGHT) #'(b< LEFT RIGHT)]
+  [(_ LEFT ">" RIGHT) #'(b> LEFT RIGHT)]
+  [(_ LEFT "<>" RIGHT) #'(b<> LEFT RIGHT)])
+
+(define-macro-cases b-if
+  [(_ COND-EXPR THEN-EXPR) #'(b-if COND-EXPR THEN-EXPR (void))]
+  [(_ COND-EXPR THEN-EXPR ELSE-EXPR)
+   #'(let ([result (if (nonzero? COND-EXPR)
+                       THEN-EXPR
+                       ELSE-EXPR)])
+       (when (exact-positive-integer? result)
+         (b-goto result)))])
