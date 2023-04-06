df0728968c6415cb261116d019d6789f576fb358
Jared Forth <jared.forth@output.com>
2023-04-06 10:26:28 -0500

    update go

--- go.rkt
+++ go.rkt
@@ -1,8 +1,21 @@
 #lang br
 (require "struct.rkt" "line.rkt")
-(provide b-end b-goto)
+(provide b-end b-goto b-gosub b-return)
 
 (define (b-end) (raise (end-program-signal)))
 
 (define (b-goto num-expr)
   (raise (change-line-signal num-expr)))
+
+(define return-ccs empty)
+
+(define (b-gosub num-expr)
+  (let/cc here-cc
+    (push! return-ccs here-cc)
+    (b-goto num-expr)))
+
+(define (b-return)
+  (when (empty? return-ccs)
+    (raise-line-error "return without gosub"))
+  (define top-cc (pop! return-ccs))
+  (top-cc (void)))
